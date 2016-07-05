20128ea0bb7d274ad2d3a378d4ea5db513fc6bcb
fiberhg <fiberhg@gmail.com>
2016-07-05 15:57:59 +0900

    t

--- SQLinjection.java
+++ SQLinjection.java
@@ -1,8 +1,8 @@
 package gitTest;
+import java.sql.PreparedStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import javax.servlet.http.HttpServletRequest;
 public class SQLinjection {
 	public static void main(HttpServletRequest request){
@@ -20,24 +20,30 @@ public class SQLinjection {
     }
 	private static void SQL(String id, String pwd) throws ClassNotFoundException, SQLException {
         
-        Connection con = null;
-        Statement stmt = null;
+		Connection con = null;
+        PreparedStatement ps = null;
         ResultSet rs = null;
-        try{
-        con = DBConnection.getConnection();
-        stmt = con.createStatement();
-        String query = "select name, country, password from Users where email = '"+id+"' and password='"+pwd+"'";
-        System.out.println(query);
-        rs = stmt.executeQuery(query);
-         
-        while(rs.next()){
-            System.out.println("Name="+rs.getString("name")+",country="+rs.getString("country")+",password="+rs.getString("password"));
-        }
-        }finally{
-            if(rs != null) rs.close();
-            stmt.close();
+        String query = "select name, country, password from Users where email = ? and password = ?";
+        try {
+            con = DBConnection.getConnection();
+            ps = con.prepareStatement(query);
+             
+            //set the parameter
+            ps.setString(1, id);
+            ps.setString(2, pwd);
+            rs = ps.executeQuery();
+ 
+            while (rs.next()) {
+                System.out.println("Name=" + rs.getString("name") + ",country="
+                        + rs.getString("country") + ",password="
+                        + rs.getString("password"));
+            }
+        } finally {
+            if (rs != null)
+                rs.close();
+            ps.close();
             con.close();
         }
-         
+ 
     }
 }
